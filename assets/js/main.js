86051842fd75eab40be7c2c4cecab8b8a6ea212e
ni.colara@hotmail.com <ni.colara@hotmail.com>
2022-02-09 20:45:00 -0300

    Se agrega funcionalidad al formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,21 +10,45 @@ $(document).ready(function () {
     );
   });
 });
+// Captura datos de formulario de contacto
+function enviar() {
+  class Persona {
+    constructor(nombre, apellido, correo, escribe) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.correo = correo;
+      this.escribe = escribe;
+    }
+  }
+  var capturarNombre = document.getElementById("nombre").value;
+  var capturarApellido = document.getElementById("apellido").value;
+  var capturarCorreo = document.getElementById("correo").value;
+  var capturarMensaje = document.getElementById("escribe").value;
 
-//Función para inicializar efecto Toolip según literatura
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="tooltip"]')
-);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
-});
+  capturarUsuario = new Persona(
+    capturarNombre,
+    capturarApellido,
+    capturarCorreo,
+    capturarMensaje
+  );
 
-//Función que realmete  inicializa  efecto Toolip
-$(document).ready(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-//Función que reemplaza color de  fondo de  tooltip
-$(document).on("inserted.bs.tooltip", function (e) {
-  var tooltip = $(e.target).data("bs.tooltip");
-  tooltip.$tip.addClass($(e.target).data("tooltip-custom-class"));
-});
+  agregar();
+}
+//Crear  array con push y variable capturarUsuario
+var baseDatos = [];
+function agregar() {
+  baseDatos.push(capturarUsuario);
+  document.getElementById("prueba").innerHTML +=
+    "<p>" +
+    capturarUsuario.nombre +
+    "</p>" +
+    "<p>" +
+    capturarUsuario.apellido +
+    "</p>" +
+    "<p>" +
+    capturarUsuario.correo +
+    "</p>" +
+    "<p>" +
+    capturarUsuario.escribe +
+    "</p>";
+}
