cf691e596e034e7e8b92c5d3c4b222665e8e5f3b
Nicole <nlara@option.cl>
2022-04-13 21:48:45 -0400

    se añaden comentarios

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,11 +1,11 @@
 // Captura datos de formulario de contacto
 
-// id boton eviar  datos para hacer confirmaciòn
+//  se captura evento mediante id boton para  eviar  datos para hacer confirmaciòn
 const buttonSend = document.getElementById("send");
 buttonSend.addEventListener("click", post, false);
 //VALIDACIÒN FORMULARIO
 const ValidationAccess = document.addEventListener(
-  "DOMContentLoaded",
+  "DOMContentLoaded", // El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado 
   function () {
     document
       .getElementById("applicationForm")
@@ -23,43 +23,46 @@ function post() {
       this.comment = comment;
     }
   }
-  if (localStorage.getItem("Users")) {
+  if (localStorage.getItem("Users")) { //  almacenar o crear datos del lado del cliente
     personDataBase = JSON.parse(localStorage.getItem("Users"));
   } else {
     localStorage.setItem("Users", JSON.stringify(personDataBase));
   }
 
-  firstName = document.getElementById("getName").value;
-  lastName = document.getElementById("getLastName").value;
-  mail = document.getElementById("getMail").value;
-  comment = document.getElementById("getComment").value;
+  firstName = document.getElementById("getName").value; // se captura el valor ingresado por el usuario (nombre)
+  lastName = document.getElementById("getLastName").value;// se captura el valor ingresado por el usuario (apellido)
+  mail = document.getElementById("getMail").value;// se captura el valor ingresado por el usuario (correo)
+  comment = document.getElementById("getComment").value; // se captura el valor ingresado por el usuario (comentario)
   capturePerson = new Person(firstName, lastName, mail, comment);
 
   localStorage.setItem("Users", JSON.stringify(personDataBase));
 }
-//Array para almacenar datos solicitados al usuario para reserva
+// se declara Array vacio para almacenar datos ingresados por el usuario 
 let personDataBase = [];
 function addData() {
-  personDataBase.push(capturePerson);
+  personDataBase.push(capturePerson); // se envian al arra PersonDataBase datos ingresados por el usuario
   swal({
-    title: "¡Hola!",
+    title: "¡Hola!",// se usa libreria sweetalert para indicar al usuario mediante una alert con estilos añadidos que pronto sera contactado
     text: ` Pronto nos pondremos en contacto contigo al correo ${capturePerson.mail} `,
     icon: "assets/img/gatapelualogo1.png",
     button: "Entendido!",
   });
 }
 
-function cleanInput() {
-  setTimeout("document.applicationForm.reset()", 100);
+function cleanInput() { // se crea función que limpiara inputs despues de haber enviado los datos del formulario
+  setTimeout("document.applicationForm.reset()", 100);// setTimeout método llama a una función después de un número de milisegundos.
   return false;
 }
 
 function validateForm(event) {
-  event.preventDefault();
+  event.preventDefault(); /* preventDefault() Cancela el evento si este es cancelable, sin detener el resto del funcionamiento del
+   evento, es decir, puede ser llamado de nuevo.*/
   let validateName = firstName;
-  if (validateName.length == 0) {
+  if (validateName.length == 0) {/* se realiza validación de ingreso de Nombre
+     indicando que si el valor/largo es 0 no se ha ingresado nombre*/
     document.getElementById("getName").focus();
-    Toastify({
+    Toastify({/* se realiza validación de ingreso de Nombre
+     indicando que si el valor/largo es 0 no se ha ingresado nombre*/
       text: "No olvides ingresar tu Nombre :D",
       position: "center", // `left`, `center` or `right`
       style: {
@@ -70,9 +73,10 @@ function validateForm(event) {
     return;
   }
   let validateLastName = lastName;
-  if (validateLastName.length == 0) {
+  if (validateLastName.length == 0) {/* se realiza validación de ingreso de apellido
+     indicando que si el valor/largo es 0 no se ha ingresado apellido*/
     document.getElementById("getLastName").focus();
-    Toastify({
+    Toastify({/*Se hace uso de libreria Toastify para indicar al usuario que debe ingresar su apellido*/
       text: "Upss no ingresaste tu Apellido",
       position: "center", // `left`, `center` or `right`
       style: {
@@ -83,9 +87,10 @@ function validateForm(event) {
     return;
   }
   let validateLastMail = mail;
-  if (validateLastMail.length == 0) {
+  if (validateLastMail.length == 0) {/* se realiza validación de ingreso de correo
+     indicando que si el valor es o no se ha ingresado correo*/
     document.getElementById("getMail").focus();
-    Toastify({
+    Toastify({/*Se hace uso de libreria Toastify para indicar al usuario que debe ingresar su correo*/
       text: "No olvides ingresar tu Correo Electrónico",
       position: "center", // `left`, `center` or `right`
       style: {
@@ -99,11 +104,11 @@ function validateForm(event) {
   let acceptTerms = document.applicationForm.terms; //acceso a check terminos y condiciones
 
   if (acceptTerms.checked == true) {
-    addData();
-    cleanInput();
+    addData(); // se llama funcion encargada de añadir datos proporcionados por el usuario a mensaje de exito
+    cleanInput(); // se llama a funcion encargada de limpiar inputs luego de enviar datos de formulario
   } else {
     //Ingresa en esta parte del ciclo cuando se hace click en boton enviar sin aceptar terminos
-    Toastify({
+    Toastify({ /*Se hace uso de libreria Toastify para indicar al usuario que debe aceptar terminos y condiciones*/
       text: "Ya casi!! Debes aceptar terminos y condiciones",
       position: "center", // `left`, `center` or `right`
       style: {
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,61 @@
-const Fragment = document.createDocumentFragment();
-const TemplateCard = document.getElementById("template-card").content;
+/*Se utiliza Fragment como una versión ligera de Document que 
+almacena un segmento de una estructura de documento compuesta de nodos como un documento estándar.*/
+const Fragment = document.createDocumentFragment(); 
+
+/*Se capturan los ID de los  elementos de html que se ocuparan para imprimir 
+en pantalla base de datos,cantidad,total, etc*/
+const TemplateCard = document.getElementById("template-card").content; // content trae el contenido del template de la tarjeta
 const templateTotal = document.getElementById("template-total").content;
 const Items = document.getElementById("Items");
 const Total = document.getElementById("printTotal");
 const TemplateShoppingCart = document.getElementById(
   "template-ShoppingCart"
 ).content;
+
+// Se declara objeto vacio con let para luego sobreescribirlo con los productos seleccionados por el cliente 
 let shoppingCart = {};
+// Eventos
 // El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
-Items.addEventListener('click', e => { ActionIncreaseDecrease(e) })
-// separa  el evento click de los botones añadir mesa
-cards.addEventListener("click", (e) => {
-  addShoppingCart(e);
-});
 document.addEventListener('DOMContentLoaded', e => { FetchData()
     if (localStorage.getItem('shoppingCart')) {
        shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'))
-        PrintShoppingCart()
+        PrintShoppingCart(e) // e para capturar los elemento que se quieran seleccionar 
     }
 }); 
-// Se pintan en pantalla cada producto con su descripción y precio
+Items.addEventListener('click', e => { ActionIncreaseDecrease(e) })
+cards.addEventListener("click", (e) => {/* se ustiliza eventdelegation  para manejar eventos de
+ manera eficiente.y detectar el id de todos los botones que nos serviran para añadir los producto al carrito*/
+  addShoppingCart(e);
+});
+
+ const BuyButton = document.querySelector(".btn-secondary");
+  BuyButton.addEventListener("click" , Buy)
+/* Se lee información base de datos de data.json con información de los productos que se pintaran 
+en pantalla utilizando fetch*/
  const FetchData = async () => {
-    const Res = await fetch('assets/js/data.json');
-    const Data = await Res.json()
+    const Res = await fetch('assets/js/data.json');// aquí se es donde se espera que se lea la información de base de datos
+    const Data = await Res.json()// Se guarda la data base de datos de productos 
     PrintShoppingCart(Data)
-    Data.forEach((arrayDataProducts) => {
+    Data.forEach((arrayDataProducts) => {  /* Se recorre y recibe base de datos y se utiliza template con 
+    queryselector que devuelve el primer elemento del documento para imprimir en pantalla
+     información proveniente de esta misma*/
       TemplateCard.querySelector("button").dataset.id = arrayDataProducts.id;
       TemplateCard.querySelector("h5").textContent = arrayDataProducts.name;
       TemplateCard.querySelector("h6").textContent = arrayDataProducts.price;
       TemplateCard.querySelector("p").textContent = arrayDataProducts.description;
       TemplateCard.querySelector("img").setAttribute("src", arrayDataProducts.img);
-      const Clone = TemplateCard.cloneNode(true);
+      const Clone = TemplateCard.cloneNode(true); // se hace la clonacion del template que esta en index.html
       Fragment.appendChild(Clone);
       cards.appendChild(Fragment);
     });
   }
 // añadir  productos  al carrito.
 function addShoppingCart(e) {
-  if (e.target.matches(".card .btn-secondary")) {
+  if (e.target.matches(".card .btn-secondary")) {/* Se pregunta si el elemento al que se le hace click contiene esta 
+    clase y si la contiene se añadira al carrito*/
     setShoppingCart(e.target.parentElement);
-    Toastify({
+    Toastify({ /*Se hace uso de libreria Toastify para indicar al usuario cuando ha realizado una acción 
+    relevante como añadir ptoductos al carrito*/
       text: "Se añadio el producto al carrito",
       position: "center", // `left`, `center` or `right`
       style: {
@@ -48,51 +64,54 @@ function addShoppingCart(e) {
       duration: 3000,
     }).showToast();
   } else {
-    e.stopPropagation();
+    e.stopPropagation();/*stopPropagation evita que el evento actual se siga propagando en las
+     fases de captura y burbujeo.*/
   }
 }
 //Pintar productos en carrito
-function setShoppingCart(object) {
+function setShoppingCart(object) { // Se recibe el objeto
   const Product = {
-    id: object.querySelector(".card .btn-secondary").dataset.id,
+    id: object.querySelector(".card .btn-secondary").dataset.id, // se accede a la id del botón de cada producto
     name: object.querySelector("h5").textContent,
     price: object.querySelector("h6").textContent,
     quantity: 1,
   };
   // Si el producto existe se aumenta producto en cantidad
-  if (shoppingCart.hasOwnProperty(Product.id)) {
-    Product.quantity = shoppingCart[Product.id].quantity + 1;
+  if (shoppingCart.hasOwnProperty(Product.id)) { // hasOwnProperty() devuelve un booleano indicando si el objeto tiene la propiedad especificada.
+    Product.quantity = shoppingCart[Product.id].quantity + 1;// se accede solo al elemento seleccionado por el usuario
   }
-  shoppingCart[Product.id] = { ...Product }; // se copia el objeto Product
-  PrintShoppingCart();
+  shoppingCart[Product.id] = { ...Product }; /*se copia el objeto Product  y se empuja al carrito 
+  si existe el prducto este se sobreescribe y si no existe lo crea */
+  PrintShoppingCart(); // se llama la función encargada de pintar el carrito
 }
 // Se imprime en carrito Data de productos seleccionados
 const PrintShoppingCart = () => {
-  Items.innerHTML = "";
+  Items.innerHTML = ""; 
 
   Object.values(shoppingCart).forEach((Product) => {
-    //Se accede a los valores del objeto
+    /*Se accede a los valores del objeto y  se usa el template de index 
+    html para crear los productos seleccionados por el usuario de manera dinamica*/
     TemplateShoppingCart.querySelector("th").textContent = Product.id;
     TemplateShoppingCart.querySelectorAll("td")[0].textContent = Product.name;
     TemplateShoppingCart.querySelectorAll("td")[1].textContent = Product.quantity;
-    TemplateShoppingCart.querySelector("span").textContent =  Product.price * Product.quantity;
+    TemplateShoppingCart.querySelector("span").textContent =  Product.price * Product.quantity; // se multiplica precio de producto por cantidad
     //botones aumentar o disminuir cantidad de algun producto seleccionado
     TemplateShoppingCart.querySelector(".btn-secondary").dataset.id = Product.id;
     TemplateShoppingCart.querySelector(".btn-danger").dataset.id = Product.id;
-    const Clone = TemplateShoppingCart.cloneNode(true);
+    const Clone = TemplateShoppingCart.cloneNode(true);// se hace la clonacion del template que esta en index.html
     Fragment.appendChild(Clone);
   });
   Items.appendChild(Fragment);
 
-  printTotal();
+  printTotal(); // se llama a función encargada de imprimir el total en pantalla
 
   localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart))
 };
-//Función para imprimir Total de suma de productos seleccionados para comprar
+//Función para imprimir Total de suma de productos seleccionados para Buy
 const printTotal = () => {
-  Total.innerHTML = "";
+  Total.innerHTML = "";/* Esta propiedad permite reiniciar/borrar el contenido que se encuentre:*/
   if (Object.keys(shoppingCart).length === 0) {
-    Total.innerHTML = ` <th scope="row" colspan="5">Tu Carrito esta vacío</th> `;
+    Total.innerHTML = ` <th scope="row" colspan="5">Tu Carrito esta vacío</th> `; // se añade a carrito mensaje carrito vacio en el caso de que asi sea mediante  innerHTML
     return;
   }
 
@@ -102,45 +121,47 @@ const printTotal = () => {
     (collector, { quantity, price }) => collector + quantity * price, 0);
   templateTotal.querySelectorAll("td")[0].textContent = TotalQuantity;
   templateTotal.querySelector("span").textContent = TotalPrice;
-  const Clone = templateTotal.cloneNode(true);
+  const Clone = templateTotal.cloneNode(true);// se hace la clonacion del template que esta en index.html
   Fragment.appendChild(Clone);
   Total.appendChild(Fragment);
-  const EmptyCart = document.getElementById("empty-cart")
-   EmptyCart.addEventListener("click",() => {
-  shoppingCart = {};
-  PrintShoppingCart();
+  const EmptyCart = document.getElementById("empty-cart")// se detecta el evento click mediante el id de botón vaciar carrito
+   EmptyCart.addEventListener("click",() => { 
+  shoppingCart = {}; // se declara objeto vacio en el caso que usuario decida vaciar carrito
+  PrintShoppingCart(); // se llama la función encargada de pintar el carrito
    })
 };
 
-const ActionIncreaseDecrease = e => {
+const ActionIncreaseDecrease = e => { /* si la clase del botón coincide con el id aumenta la cantidad del producto
+viendose reflejada en el carrito modificando el total segun la cantidad del producto*/
     if (e.target.classList.contains('btn-secondary')) {
         const Product = shoppingCart[e.target.dataset.id]
         Product.quantity++
-        shoppingCart[e.target.dataset.id] = { ...Product }
-         PrintShoppingCart();
+        shoppingCart[e.target.dataset.id] = { ...Product } /*se copia el objeto Product  y se empuja al carrito 
+  si existe el prducto este se sobreescribe */
+         PrintShoppingCart(); // se llama la función encargada de pintar el carrito
     }
   
-    if (e.target.classList.contains('btn-danger')) {
+    if (e.target.classList.contains('btn-danger')) { /* si la clase del botón coincide con el id disminuye la cantidad del producto
+viendose reflejada en el carrito modificando el total segun la cantidad del producto*/
         const Product= shoppingCart[e.target.dataset.id]
         Product.quantity--
         if (Product.quantity === 0) {
-            delete shoppingCart[e.target.dataset.id]
+            delete shoppingCart[e.target.dataset.id]// El operador delete  elimina una propiedad de un objeto.
         } else {
           shoppingCart[e.target.dataset.id] = {...Product}
         }
-         PrintShoppingCart()
+         PrintShoppingCart() // se llama la función encargada de pintar el carrito
     }
-    e.stopPropagation()
+    e.stopPropagation()// se llama a función encargada de imprimir carrito
   }
 
-  const ComprarButon = document.querySelector(".btn-secondary");
-  ComprarButon.addEventListener("click" , comprar)
-
-  function comprar() {
+ 
+  function Buy() {    /*Función que permite mostrar un alert con libreria sweetalert indicando al usuario 
+  un mensaje compra exitosa si se detecta el evento click de botón comprar  */
     Items.innerHTML= "";
     Total.innerHTML= "";
-    if(comprar) {
-       swal({
+    if(Buy) {
+       swal({  // se usa libreria sweetalert para indicar al usuario mediante una alert con estilos añadidos que su compra fue exitosa
     title: "¡Compra exitosa!",
     text: ` Recibiras tu compra dentro de los 3 siguientes dias habiles `,
     icon: "assets/img/gatapelualogo1.png",
